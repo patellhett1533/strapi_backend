4733eb4f6602c4ca60bdc98e0517b95caf0e8d26
het patel <hppatidar12d@gmail.com>
2024-10-05 19:17:22 +0530

    update a all field populated

--- src/api/page-content/controllers/page-content.ts
+++ src/api/page-content/controllers/page-content.ts
@@ -2,6 +2,49 @@
  * page-content controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::page-content.page-content');
+export default factories.createCoreController(
+  "api::page-content.page-content",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const query = {
+        ...ctx.query,
+        populate: {
+          reason_data: {
+            populate: "*",
+          },
+        },
+      };
+
+      // Call the default Strapi entity service with the updated query
+      const entities = await strapi.entityService.findMany(
+        "api::page-content.page-content",
+        query as any
+      );
+
+      return entities;
+    },
+
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const query = {
+        ...ctx.query,
+        populate: {
+          reason_data: {
+            populate: "*", // This will populate all fields in the reason_data component
+          },
+        },
+      };
+
+      // Call the default Strapi entity service with the updated query
+      const entity = await strapi.entityService.findOne(
+        "api::page-content.page-content",
+        id,
+        query as any
+      );
+
+      return entity;
+    },
+  })
+);
